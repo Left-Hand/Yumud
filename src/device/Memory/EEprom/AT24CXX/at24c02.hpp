d5f341082692431139336bf299025b042839158a
Rstr1aN <745236859@qq.com>
2024-05-13 02:25:12 +0800

    add dur protect

--- src/device/Memory/EEprom/AT24CXX/at24c02.hpp
+++ src/device/Memory/EEprom/AT24CXX/at24c02.hpp
@@ -14,10 +14,11 @@
 class AT24C02:public Storage{
 protected:
     I2cDrv & bus_drv;
-    const Address chip_size = 256;
-    const uint32_t max_page = 8;
-
-    // void _store(const void * data, const Address & data_size, const Address & loc) override;
+    static constexpr Address chip_size = 256;
+    static constexpr uint32_t max_page = 8;
+    static constexpr uint32_t min_duration_ms = 5;
+    uint32_t last_entry_ms = 0;
+    
     void _store(const uint8_t & data, const Address & loc) override{
         if(loc >= chip_size){
             AT24C02_DEBUG("AT24C02: writeData: invalid addr or len");
@@ -38,39 +39,19 @@ protected:
     }
     // void _load(void * data, const Address & data_size, const Address & loc) = 0;
 
-    void entry() override{};
-    void exit() override{};
+    void entry() override{
+        delay(MAX(last_entry_ms + min_duration_ms - millis(), 0));
+        last_entry_ms = millis();
+    };
+
+    void exit() override{
+        last_entry_ms = millis();
+    };
 public:
     void init() override{};
 
     static constexpr uint8_t default_id = 0b10100000; 
     AT24C02(I2cDrv & _bus_drv):Storage(256), bus_drv(_bus_drv){;}
-
-    // void writeData(const uint8_t & addr, const uint8_t * data, uint8_t len){
-    //     if(addr + len > chip_size || len > max_page){
-    //         AT24C02_DEBUG("AT24C02: writeData: invalid addr or len");
-    //         len = MIN(chip_size, addr + len) - addr;
-    //     }
-    //     bus_drv.writePool(addr, data, 1, len);
-    // }
-
-    // void writeData(const uint8_t & addr, const uint8_t & data){
-    //     AT24C02_DEBUG("AT24C02 write: ", addr, data);
-    //     writeData(addr, &data, 1);
-    // }
-
-    // void readData(const uint8_t & addr, uint8_t * data, uint8_t len){
-    //     if(addr + len > chip_size || len > max_page){
-    //         AT24C02_DEBUG("AT24C02: readData: invalid addr or len");
-    //         len = MIN(chip_size, addr + len) - addr;
-    //     }
-    //     bus_drv.readPool(addr, data, 1, len);
-    // }
-
-    // void readData(const uint8_t & addr, uint8_t * data){
-    //     AT24C02_DEBUG("AT24C02 read: ", addr, *data);
-    //     readData(addr, data, 1);
-    // }
 };
 
 #endif
