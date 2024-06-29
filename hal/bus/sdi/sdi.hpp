ae368d6a2d372156b87a780dfe52fb9af47ab487
Rstr1aN <745236859@qq.com>
2024-06-29 20:18:07 +0800

    [feat] sdi & dvp

---
+++ hal/bus/sdi/sdi.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../bus.hpp"
+#include "sys/kernel/stream.hpp"
+
+class Sdi:public OutputStream{
+
+public:
+
+    void write(const char data) override;
+
+    void write(const char * data_ptr, const size_t len) override;
+
+    size_t pending() const override{
+        return 0;
+    }
+
+
+
+    void init();
+};
