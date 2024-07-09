4e70cf23b7dc63b9c2340f0cadbd0091fd763d65
Rstr1aN <745236859@qq.com>
2024-07-09 09:44:32 +0800

    fix devices

--- drivers/Proximeter/VL53L0X/vl53l0x.hpp
+++ drivers/Proximeter/VL53L0X/vl53l0x.hpp
@@ -1,16 +1,16 @@
-#ifndef __VL53L0X_H
-#define __VL53L0X_H
+#pragma once
 
-#include "bus/i2c/i2cdrv.hpp"
+#include "hal/bus/i2c/i2cdrv.hpp"
 
-#define VL53L0X_DEF_I2C_ADDR 0x29
 
 class VL53L0X{
 protected:
-    I2cDrv & bus_drv;
+    I2cDrv bus_drv;
 public:
     static constexpr uint8_t default_id = 0x52;
     VL53L0X(I2cDrv & _bus_drv):bus_drv(_bus_drv){;}
+    VL53L0X(I2cDrv && _bus_drv):bus_drv(_bus_drv){;}
+    VL53L0X(I2c & bus):bus_drv(bus, default_id){;}
     ~VL53L0X(){;}
 
     void startConv();
@@ -28,13 +28,16 @@ private:
     bool highPrec = false;
     bool continuous = false;
 
+    #pragma pack(push, 1)
+
     struct Result{
         uint16_t ambientCount; /**< Environment quantity */
         uint16_t signalCount;  /**< A semaphore */
         uint16_t distance;
     };
 
-    // uint16_t last_distance;
+    #pragma pack(pop)
+
     Result result, last_result;
 	void writeByteData(const uint8_t Reg, const uint8_t byte){
         bus_drv.writeReg(Reg, byte);
@@ -53,7 +56,3 @@ private:
         bus_drv.readPool(reg, data, len);
     }
 };
-
-#endif
-
-
