62de8d6f9c212edaeecd7c8070c5939dbb68e60a
Rstr1aN <745236859@qq.com>
2024-09-25 00:09:48 +0800

    update

--- drivers/Actuator/DJI/M3508/m3508.cpp
+++ drivers/Actuator/DJI/M3508/m3508.cpp
@@ -1,13 +1,102 @@
 #include "m3508.hpp"
 
-void M3508Port::M3508::tick(){
+using M3508 = M3508Port::M3508;
 
+void M3508::init(){
+    odo_.init();
 }
 
-void M3508Port::M3508::setTargetSpeed(const real_t spd){
+void M3508::reset(){
+    odo_.reset();
+
+    lap_position = 0;
+    curr = 0;
+    speed = 0;
+    temperature = 0;
+
+    spd_pid = {3, 0, 0};
+    pos_pid = {3, 0, 0};
+
+    ctrl_method = CtrlMethod::NONE;
     
+    targ_curr = 0;
+    targ_spd = 0;
+    targ_pos = 0;
+
+    curr_delta = real_t(0.02);
+    curr_setpoint = 0;
 }
 
-void M3508Port::M3508::setTargetPosition(const real_t pos){
+
+void M3508::tick(){
+    odo_.update();
+
+    real_t expect_curr;
+    switch(ctrl_method){
+        default:
+        case CtrlMethod::NONE:
+            break;
+        case CtrlMethod::CURR:
+            expect_curr = targ_curr;
+            break;
+        case CtrlMethod::SPD:
+            expect_curr = spd_pid.update(targ_spd, getSpeed());
+            break;
+        case CtrlMethod::POS:
+            expect_curr = pos_pid.update(targ_pos, getPosition());
+            break;
+    }
+
+    curr_setpoint = STEP_TO(curr_setpoint, expect_curr, curr_delta);
+    applyTargetCurrent(curr_setpoint);
+}
+
+void M3508::setTargetCurrent(const real_t curr){
+    ctrl_method = CtrlMethod::CURR;
+    targ_curr = curr;
+}
+
+
+void M3508::setTargetSpeed(const real_t spd){
+    ctrl_method = CtrlMethod::SPD;
+    targ_spd = spd;
+}
+
+void M3508::setTargetPosition(const real_t pos){
+    targ_pos = pos;
+}
+
+void M3508Port::init(){
+    for(auto & inst : inst_){
+        inst.init();
+    }
+}
+
+
+void M3508Port::reset(){
+    for(auto & inst : inst_){
+        inst.reset();
+    }
+}
+
+void M3508Port::tick(){
+    while(can.available()){
+        const auto & msg = can.front();
+        if(msg.id() > 0x200 and msg.id() <= 0x208){
+            can.read();
+            size_t index = msg.id() - 0x200;
+            updateInst(msg, index);
+        }
+    }
+
     
+    if(size > 0){
+        while(can.pending() >= 3);
+        can.write(CanMsg{0x200, tx_datas[0]});
+    }
+
+    if(size > 4){
+        while(can.pending() >= 3);
+        can.write(CanMsg{0x1FF, tx_datas[1]});
+    }
 }
--- drivers/Actuator/DJI/M3508/m3508.hpp
+++ drivers/Actuator/DJI/M3508/m3508.hpp
@@ -1,8 +1,87 @@
 #pragma once
 
 #include "hal/bus/can/can.hpp"
+#include "drivers/Encoder/odometer.hpp"
+#include "dsp/controller/PID.hpp"
 
 class M3508Port{
+public:
+    class M3508{
+    protected:
+        M3508Port & port;
+        const size_t index;
+        
+        real_t lap_position = 0;
+        real_t curr = 0;
+        real_t speed = 0;
+        real_t temperature = 0;
+
+        using PID = PID_t<real_t>;
+
+        PID spd_pid = {3, 0, 0};
+        PID pos_pid = {3, 0, 0};
+
+        enum class CtrlMethod:uint8_t{
+            NONE,
+            CURR,
+            SPD,
+            POS
+        };
+
+
+        CtrlMethod ctrl_method = CtrlMethod::NONE;
+        
+        real_t targ_curr = 0;
+        real_t targ_spd = 0;
+        real_t targ_pos = 0;
+
+        real_t curr_delta = real_t(0.02);
+        real_t curr_setpoint = 0;
+
+
+        class M3508Encoder : public Encoder{
+        protected:
+            M3508 & owner;
+
+        public:
+            M3508Encoder(M3508 & _owner):owner(_owner){;}
+            virtual void init() override {}
+            virtual void update() override{}
+            virtual real_t getLapPosition() override {return owner.lap_position;}
+            virtual bool stable() override {return true;}
+        };
+
+        M3508Encoder enc_{*this};
+        Odometer odo_{enc_};
+        
+        M3508(M3508Port & _port, const size_t _index):
+            port(_port), index(_index){reset();}
+
+        auto & operator = (const M3508 & other) = delete;
+        auto & operator = (M3508 && other) = delete;
+
+
+        friend class M3508Port;
+        friend class M3508Encoder;
+
+        void applyTargetCurrent(const real_t curr){port.setTargetCurrent(curr, index);}
+    public:
+        void init();
+        void tick();
+        void reset();
+        void setTargetCurrent(const real_t curr);
+        void setTargetSpeed(const real_t spd);
+        void setTargetPosition(const real_t pos);
+
+        real_t getPosition() {return odo_.getPosition();}
+        real_t getCurrent() const {return curr;}
+        real_t getSpeed() const {return speed;}
+        real_t getTemperature() const {return temperature;}
+
+        auto & enc() {return enc_;}
+        auto & odo() {return odo_;}
+    };
+protected:
     #define M3508_CHECK_INDEX if(index > size or index == 0) CREATE_FAULT; 
     scexpr size_t max_size = 8;
     
@@ -34,42 +113,17 @@ class M3508Port{
     };
 
     
-    class M3508{
-    protected:
-        M3508Port & port;
-        const size_t index;
-        
-        real_t lap_position = 0;
-        real_t curr = 0;
-        real_t speed = 0;
-        real_t temperature = 0;
-        
-        friend class M3508Port;
-        M3508(M3508Port & _port, const size_t _index):
-            port(_port), index(_index){;}
 
-        auto & operator = (const M3508 & other) = delete;
-        auto & operator = (M3508 && other) = delete;
-    public:
-
-        void tick();
-        void setTargetCurrent(const real_t curr){port.setTargetCurrent(curr, index);}
-        void setTargetSpeed(const real_t spd);
-        void setTargetPosition(const real_t pos);
-        real_t getCurrent() const {return curr;}
-        real_t getSpeed() const {return speed;}
-        real_t getTemperature() const {return temperature;}
-    };
 
     std::array<M3508, max_size> inst_ = {
-        M3508(*this, 1),
-        M3508(*this, 2),
-        M3508(*this, 3),
-        M3508(*this, 4),
-        M3508(*this, 5),
-        M3508(*this, 6),
-        M3508(*this, 7),
-        M3508(*this, 8),
+        M3508{*this, 1},
+        M3508{*this, 2},
+        M3508{*this, 3},
+        M3508{*this, 4},
+        M3508{*this, 5},
+        M3508{*this, 6},
+        M3508{*this, 7},
+        M3508{*this, 8},
     };
     
     union{
@@ -81,13 +135,13 @@ class M3508Port{
     
     void setTargetCurrent(const real_t curr, const size_t index){
         M3508_CHECK_INDEX
-        curr_cache[index] = curr_to_currdata(curr);
+        curr_cache[index - 1] = curr_to_currdata(curr);
     }
 
     void updateInst(const CanMsg & msg, const size_t index){
         M3508_CHECK_INDEX
         auto rx_data = RxData(msg);
-        auto & inst = inst_[index];
+        auto & inst = inst_[index - 1];
 
         inst.lap_position = rx_data.angle_8192 / 8192;
         inst.curr = currdata_to_curr(rx_data.curr_data);
@@ -97,29 +151,13 @@ class M3508Port{
         inst.tick();
     }
 public:
-    M3508Port(Can & _can):can(_can){}
+    M3508Port(Can & _can):can(_can){reset();}
+    
+    void init();
 
-    void tick(){
-        while(can.available()){
-            const auto & msg = can.front();
-            if(msg.id() > 0x200 and msg.id() <= 0x208){
-                can.read();
-                size_t index = msg.id() - 0x200 - 1;
-                M3508_CHECK_INDEX
-                updateInst(msg, index);
-            }
-        }
+    void reset();
 
-        if(can.pending() < 2){
-            if(size > 0){
-                can.write(CanMsg{0x200, tx_datas[0]});
-            }
-
-            if(size > 4){
-                can.write(CanMsg{0x1FF, tx_datas[1]});
-            }
-        }
-    }
+    void tick();
 
     void setSize(const size_t _size){
         if(size > max_size) CREATE_FAULT
