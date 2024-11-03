2334abe4f6560ea7020904a2309f337ff757083f
Rstr1aN <745236859@qq.com>
2024-11-04 01:03:25 +0800

    update icm42688

--- drivers/IMU/Axis6/ICM42688/icm42688.cpp
+++ drivers/IMU/Axis6/ICM42688/icm42688.cpp
@@ -2,12 +2,110 @@
 
 using namespace yumud::drivers;
 
-void ICM42688::init(){
+scexpr real_t LSB_ACC_16G_x64 = 64 *  0.0047856934_r;
+scexpr real_t  LSB_ACC_8G_x64 = 64 *  0.0023928467_r;
+scexpr real_t  LSB_ACC_4G_x64 = 64 *  0.0011964233_r;
+scexpr real_t  LSB_ACC_2G_x64 = 64 * 0.00059821167_r;
 
+/*Turn Into Radian*/
+scexpr real_t LSB_GYRO_2000_R_x256	 = 256 * 0.0010652644_r;
+scexpr real_t LSB_GYRO_1000_R_x256	= 256 * 0.00053263222_r;
+scexpr real_t  LSB_GYRO_500_R_x256	= 256 * 0.00026631611_r;
+scexpr real_t  LSB_GYRO_250_R_x256	= 256 * 0.00013315805_r;
+scexpr real_t LSB_GYRO_125D_R_x256  = 256 * 0.000066579027_r;	
+
+
+
+void ICM42688::init(){
+	/*指定Bank0*/
+	writeReg(0x76,0x00);
+	/*软重启*/
+	writeReg(0x11,0x01);
+	delay(30);
+	/*读取中断位 切换SPI*/
+//	buf = IMU->ReadReg(0x2D);
+	/*指定Bank0*/
+	writeReg(0x76,0x00);
+	/*Gyro设置*/
+	writeReg(0x4F,0x06);//2000dps 1KHz
+	/*Accel设置*/
+	writeReg(0x50,0x06);//16G 1KHz
+	/*电源管理*/
+	writeReg(0x4E,0x0F);//ACC GYRO LowNoise Mode
+	
+	delay(30);
+
+	
+	/*指定Bank0*/
+	writeReg(0x76,0x00);
+	/*中断输出设置*/
+//	writeReg(0x14,0x12);//INT1 INT2 脉冲模式，低有效
+	/*Gyro设置*/
+	writeReg(0x4F,0x06);//2000dps 1KHz
+	/*Accel设置*/
+	writeReg(0x50,0x06);//16G 1KHz
+	/*LSB设置*/
+	lsb_acc_x64 = LSB_ACC_16G_x64;
+	lsb_gyro_x256 = LSB_GYRO_2000_R_x256;
+	/*Tem设置&Gyro_Config1*/
+	writeReg(0x51,0x56);//BW 82Hz Latency = 2ms
+	/*GYRO_ACCEL_CONFIG0*/
+	writeReg(0x52,0x11);//1BW
+	/*ACCEL_CONFIG1*/
+	writeReg(0x53,0x0D);//Null
+	/*INT_CONFIG0*/
+	writeReg(0x63,0x00);//Null
+	/*INT_CONFIG1*/
+//	writeReg(0x64,0x00);//中断引脚正常启用
+	/*INT_SOURCE0*/
+	writeReg(0x65,0x08);//DRDY INT1
+	/*INT_SOURCE1*/
+	writeReg(0x66,0x00);//Null
+	/*INT_SOURCE3*/
+	writeReg(0x68,0x00);//Null
+	/*INT_SOURCE3*/
+	writeReg(0x69,0x00);//Null
+	
+/*****抗混叠滤波器@536Hz*****/
+	
+	/*GYRO抗混叠滤波器配置*/
+	/*指定Bank1*/
+	writeReg(0x76,0x01);
+	/*GYRO抗混叠滤波器配置*/
+	writeReg(0x0B,0xA0);//开启抗混叠和陷波滤波器
+	writeReg(0x0C,0x0C);//GYRO_AAF_DELT 12 (default 13)
+	writeReg(0x0D,0x90);//GYRO_AAF_DELTSQR 144 (default 170)
+	writeReg(0x0E,0x80);//GYRO_AAF_BITSHIFT 8 (default 8)
+	
+	/*ACCEL抗混叠滤波器配置*/
+	/*指定Bank2*/
+	writeReg(0x76,0x02);
+	/*ACCEL抗混叠滤波器配置*/
+	writeReg(0x03,0x18);//开启滤波器 ACCEL_AFF_DELT 12 (default 24)
+	writeReg(0x04,0x90);//ACCEL_AFF_DELTSQR 144 (default 64)
+	writeReg(0x05,0x80);//ACCEL_AAF_BITSHIFT 8 (default 6)
+
+/*****自定义滤波器1号@111Hz*****/
+
+	/*指定Bank0*/
+	writeReg(0x76,0x00);
+	/*滤波器顺序*/
+	writeReg(0x51,0x12);//GYRO滤波器1st
+	writeReg(0x53,0x05);//ACCEL滤波器1st
+	/*滤波器设置*/
+	writeReg(0x52,0x33);//111Hz 03
+	/*指定Bank0*/
+	writeReg(0x76,0x00);
+	/*电源管理*/
+	writeReg(0x4E,0x0F);//ACC GYRO LowNoise Mode
 }
 
 void ICM42688::update(){
+	// requestData(uint8_t(RegAddress::ACCEL_DATA_X1), &accel_data.x, 3);
+	requestData(uint8_t(RegAddress::ACCEL_DATA_X1), &accel_data.x, 6);
 
+
+	// requestData(uint8_t(RegAddress::GYRO_DATA_X1), &gyro_data.x, 3);
 }
 
 
@@ -20,10 +118,20 @@ void ICM42688::reset(){
 }
 
 std::tuple<real_t, real_t, real_t> ICM42688::getAccel(){
-    return {0,0,0};
+
+    auto conv = [this](const real_t x) -> real_t {
+        return ((x * lsb_acc_x64) >> 6);
+    };
+
+    return {conv(accel_data.x), conv(accel_data.y),conv(accel_data.z)};
 }
 
 
 std::tuple<real_t, real_t, real_t> ICM42688::getGyro(){
-    return {0,0,0};
+
+    auto conv = [this](const real_t x) -> real_t {
+        return ((x * this->lsb_gyro_x256) >> 8);
+    };
+
+    return {conv(gyro_data.x), conv(gyro_data.y),conv(gyro_data.z)};
 }
--- drivers/IMU/Axis6/ICM42688/icm42688.hpp
+++ drivers/IMU/Axis6/ICM42688/icm42688.hpp
@@ -21,16 +21,154 @@ namespace yumud::drivers{
 class ICM42688:public Axis6{
 public:
 
+    enum class AFS:uint8_t{
+        _16G,// default
+        _8G,
+        _4G,
+        _2G,
+    };
+    enum class AODR:uint8_t
+    {
+        _32000HZ,
+        _16000HZ,
+        _8000HZ,
+        _4000HZ,
+        _2000HZ,
+        _1000HZ,// default
+        _200HZ,
+        _100HZ,
+        _50HZ,
+        _25HZ,
+        _12_5HZ,
+        _6_25HZ,
+        _3_125HZ,
+        _1_5625HZ,
+        _500HZ,
+    };
 
+    enum class GFS:uint8_t{
+        _2000DPS,// default
+        _1000DPS,
+        _500DPS,
+        _250DPS,
+        _125DPS,
+        _62_5DPS,
+        _31_25DPS,
+        _15_625DPS,
+    };
+
+
+    enum class GODR:uint8_t{
+        _32000HZ,
+        _16000HZ,
+        _8000HZ,
+        _4000HZ,
+        _2000HZ,
+        _1000HZ,// default
+        _200HZ,
+        _100HZ,
+        _50HZ,
+        _25HZ,
+        _12_5HZ,
+        _X0HZ,
+        _X1HZ,
+        _X2HZ,
+        _500HZ,
+    };
+
+    enum class RegAddress:uint8_t{
+        ID = 0x47,
+        DEVICE_CONFIG             = 0x11,
+        DRIVE_CONFIG              = 0x13,
+        INT_CONFIG                = 0x14,
+        FIFO_CONFIG               = 0x16,
+        TEMP_DATA1                = 0x1D,
+        TEMP_DATA0                = 0x1E,
+        ACCEL_DATA_X1             = 0x1F,
+        ACCEL_DATA_X0             = 0x20,
+        ACCEL_DATA_Y1             = 0x21,
+        ACCEL_DATA_Y0             = 0x22,
+        ACCEL_DATA_Z1             = 0x23,
+        ACCEL_DATA_Z0             = 0x24,
+        GYRO_DATA_X1              = 0x25,
+        GYRO_DATA_X0              = 0x26,
+        GYRO_DATA_Y1              = 0x27,
+        GYRO_DATA_Y0              = 0x28,
+        GYRO_DATA_Z1              = 0x29,
+        GYRO_DATA_Z0              = 0x2A,
+        TMST_FSYNCH               = 0x2B,
+        TMST_FSYNCL               = 0x2C,
+        INT_STATUS                = 0x2D,
+        FIFO_COUNTH               = 0x2E,
+        FIFO_COUNTL               = 0x2F,
+        FIFO_DATA                 = 0x30,
+        APEX_DATA0                = 0x31,
+        APEX_DATA1                = 0x32,
+        APEX_DATA2                = 0x33,
+        APEX_DATA3                = 0x34,
+        APEX_DATA4                = 0x35,
+        APEX_DATA5                = 0x36,
+        INT_STATUS2               = 0x37,
+        INT_STATUS3               = 0x38,
+        SIGNAL_PATH_RESET         = 0x4B,
+        INTF_CONFIG0              = 0x4C,
+        INTF_CONFIG1              = 0x4D,
+        PWR_MGMT0                 = 0x4E,
+        GYRO_CONFIG0              = 0x4F,
+        ACCEL_CONFIG0             = 0x50,
+        GYRO_CONFIG1              = 0x51,
+        GYRO_ACCEL_CONFIG0        = 0x52,
+        ACCEL_CONFIG1             = 0x53,
+        TMST_CONFIG               = 0x54,
+        APEX_CONFIG0              = 0x56,
+        SMD_CONFIG                = 0x57,
+        FIFO_CONFIG1              = 0x5F,
+        FIFO_CONFIG2              = 0x60,
+        FIFO_CONFIG3              = 0x61,
+        FSYNC_CONFIG              = 0x62,
+        INT_CONFIG0               = 0x63,
+        INT_CONFIG1               = 0x64,
+        INT_SOURCE0               = 0x65,
+        INT_SOURCE1               = 0x66,
+        INT_SOURCE3               = 0x68,
+        INT_SOURCE4               = 0x69,
+        FIFO_LOST_PKT0            = 0x6C,
+        FIFO_LOST_PKT1            = 0x6D,
+        SELF_TEST_CONFIG          = 0x70,
+        WHO_AM_I                  = 0x75,
+        REG_BANK_SEL              = 0x76,
+        SENSOR_CONFIG0            = 0x03,
+        GYRO_CONFIG_STATIC2       = 0x0B,
+        GYRO_CONFIG_STATIC3       = 0x0C,
+        GYRO_CONFIG_STATIC4       = 0x0D,
+        GYRO_CONFIG_STATIC5       = 0x0E,
+        GYRO_CONFIG_STATIC6       = 0x0F,
+        GYRO_CONFIG_STATIC7       = 0x10,
+        GYRO_CONFIG_STATIC8       = 0x11,
+        GYRO_CONFIG_STATIC9       = 0x12,
+        GYRO_CONFIG_STATIC10      = 0x13,
+        XG_ST_DATA                = 0x5F,
+        YG_ST_DATA                = 0x60,
+        ZG_ST_DATA                = 0x61,
+        TMSTVAL0                  = 0x62,
+        TMSTVAL1                  = 0x63,
+        TMSTVAL2                  = 0x64,
+        INTF_CONFIG4              = 0x7A,
+        INTF_CONFIG5              = 0x7B,
+        INTF_CONFIG6              = 0x7C
+    };
 protected:
     std::optional<I2cDrv> i2c_drv;
     std::optional<SpiDrv> spi_drv;
 
-    using RegAddress = uint8_t;
+    // using RegAddress = uint8_t;
 
 
     scexpr uint8_t default_chip_id = 0;//TODO
     scexpr uint8_t default_i2c_addr = 0x68;
+
+    real_t lsb_acc_x64;
+    real_t lsb_gyro_x256;
 
     struct Vec3i16{
         int16_t x;
@@ -44,54 +182,35 @@ protected:
     };
 
 
-    void writeReg(const uint8_t addr, const uint8_t data){
+    template<typename T = uint8_t>
+    void writeReg(const uint8_t addr, const T data){
         if(i2c_drv) i2c_drv->writeReg(addr, data, MSB);
         if(spi_drv){
             spi_drv->write(uint8_t(addr), false);
             spi_drv->write(data);
 
             ICM42688_DEBUG("Wspi", addr, data);
-
         }
     }
 
-    void readReg(const RegAddress addr, uint8_t & data){
+    template<typename T = uint8_t>
+    void readReg(const uint8_t addr, T & data){
         if(i2c_drv) i2c_drv->readReg((uint8_t)addr, data, MSB);
         if(spi_drv){
-            spi_drv->write(uint8_t(uint8_t(addr) | 0x80), false);
+            spi_drv->write(uint8_t(uint8_t(addr) | 0x80), CONT);
             spi_drv->read(data);
-        }
 
-        ICM42688_DEBUG("Rspi", addr, data);
-    }
-
-
-    void writeReg(const uint8_t addr, const uint16_t data){
-        if(i2c_drv) i2c_drv->writeReg(addr, data, MSB);
-        if(spi_drv){
-            spi_drv->write(uint8_t(addr), false);
-            spi_drv->write(data);
-
-            ICM42688_DEBUG("Wspi", addr, data);
-
+            ICM42688_DEBUG("Rspi", addr, data);
         }
     }
 
-    void readReg(const RegAddress addr, uint16_t & data){
-        if(i2c_drv) i2c_drv->readReg((uint8_t)addr, data, MSB);
-        if(spi_drv){
-            spi_drv->write(uint8_t(uint8_t(addr) | 0x80), false);
-            spi_drv->read(data);
-        }
 
-        ICM42688_DEBUG("Rspi", addr, data);
-    }
-
-    void requestData(const RegAddress addr, void * datas, const size_t len){
-        if(i2c_drv) i2c_drv->readPool(uint8_t(addr), (uint8_t *)datas, len, MSB);
+    template<typename T = uint8_t>
+    void requestData(const uint8_t addr, T * datas, const size_t len){
+        if(i2c_drv) i2c_drv->readPool(uint8_t(addr), datas, len, MSB);
         if(spi_drv){
-            spi_drv->write(uint8_t(uint8_t(addr) | 0x80), false);
-            spi_drv->read((uint8_t *)(datas), len);
+            spi_drv->write(uint8_t(uint8_t(addr) | 0x80), CONT);
+            spi_drv->read((datas), len);
         }
 
         ICM42688_DEBUG("Rspi", addr, len);
