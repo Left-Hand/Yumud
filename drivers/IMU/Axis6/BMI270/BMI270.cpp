8a9b8a7c2b3e466f7f821eb89d6e93a495fe4178
Rstr1aN <745236859@qq.com>
2024-11-07 21:34:17 +0800

    update

---
+++ drivers/IMU/Axis6/BMI270/BMI270.cpp
@@ -0,0 +1,34 @@
+#include "BMI270.hpp"
+
+using namespace yumud::drivers;
+
+void BMI270::writeReg(const uint8_t addr, const uint8_t data){
+    if(i2c_drv_) i2c_drv_->writeReg(addr, data, MSB);
+    if(spi_drv_){
+        spi_drv_->writeSingle(uint8_t(addr), CONT);
+        spi_drv_->writeSingle(data);
+
+        BMI270_DEBUG("Wspi", addr, data);
+
+    }
+}
+
+void BMI270::readReg(const RegAddress addr, uint8_t & data){
+    if(i2c_drv_) i2c_drv_->readReg((uint8_t)addr, data, MSB);
+    if(spi_drv_){
+        spi_drv_->writeSingle(uint8_t(uint8_t(addr) | 0x80), CONT);
+        spi_drv_->readSingle(data);
+    }
+
+    BMI270_DEBUG("Rspi", addr, data);
+}
+
+void BMI270::requestData(const RegAddress addr, void * datas, const size_t len){
+    if(i2c_drv_) i2c_drv_->readMulti(uint8_t(addr), (uint8_t *)datas, len, MSB);
+    if(spi_drv_){
+        spi_drv_->writeSingle(uint8_t(uint8_t(addr) | 0x80), CONT);
+        spi_drv_->readMulti((uint8_t *)(datas), len);
+    }
+
+    BMI270_DEBUG("Rspi", addr, len);
+}
