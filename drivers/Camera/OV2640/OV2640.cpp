fd851d7f27331acfc9c3a53fa9e00e2d7f31243a
Rstr1aN <745236859@qq.com>
2024-11-08 16:21:19 +0800

    update

--- drivers/Camera/OV2640/OV2640.cpp
+++ drivers/Camera/OV2640/OV2640.cpp
@@ -1,3 +1,12 @@
 #include "OV2640.hpp"
 
 using namespace yumud::drivers;
+
+
+bool OV2640::init(){
+    
+}
+
+bool OV2640::verify(){
+
+}
--- drivers/Camera/camera.hpp
+++ drivers/Camera/camera.hpp
@@ -1,51 +1,12 @@
 #pragma once
 
 #include "hal/bus/dvp/dvp.hpp"
-#include "hal/bus/i2c/i2cdrv.hpp"
-#include "hal/gpio/gpio.hpp"
-
-#include "types/vector2/vector2_t.hpp"
+#include "SccbDrv.hpp"
 #include "types/image/image.hpp"
 #include "types/rgb.h"
 
 namespace yumud{
 
-class SccbDrv:public I2cDrv{
-protected:
-    using BusDrv<I2c>::index;
-    using BusDrv<I2c>::bus;
-public:
-    SccbDrv(I2c & _bus, const uint8_t _index):
-        I2cDrv(_bus, _index){};
-
-    void writeSccbReg(const uint8_t reg_address, const uint16_t reg_data){
-        if(!bus.begin(index)){
-            bus.write(reg_address);
-            bus.write((uint8_t)(reg_data >> 8));
-            bus.begin(index);
-            bus.write(0xF0);
-            bus.write((uint8_t)reg_data);
-            bus.end();
-        }
-    }
-
-    void readSccbReg(const uint8_t reg_address, uint16_t & reg_data){
-        if(!bus.begin(index)){
-            uint32_t data_l, data_h;
-            bus.write(reg_address);
-            bus.begin(index | 0x01);
-            bus.read(data_h, false);
-
-            bus.begin(index);
-            bus.write(0xF0);
-            bus.begin(index | 0x01);
-            bus.read(data_l, false);
-            bus.end();
-
-            reg_data = ((uint8_t)data_h << 8) | (uint8_t)data_l;
-        }
-    }
-};
 
 template<typename ColorType>
 class CameraWithSccb:public Camera<ColorType>{
@@ -53,16 +14,9 @@ public:
     using Vector2 = ImageBasics::Vector2;
     using Vector2i = ImageBasics::Vector2i;
 protected:
-    SccbDrv bus_drv;
-    CameraWithSccb(const SccbDrv & _bus_drv, const Vector2i & _size):ImageBasics(_size), Camera<ColorType>(_size), bus_drv(_bus_drv){;}
-
-    void writeReg(const uint8_t addr, const uint16_t data){
-        bus_drv.writeSccbReg(addr, data);
-    }
-
-    void readReg(const uint8_t addr, uint16_t & data){
-        bus_drv.writeSccbReg(addr, data);
-    }
+    SccbDrv sccb_drv_;
+    CameraWithSccb(const SccbDrv & _bus_drv, const Vector2i & _size):ImageBasics(_size), Camera<ColorType>(_size), sccb_drv_(_bus_drv){;}
+    // CameraWithSccb(SccbDrv && _bus_drv, const Vector2i & _size):ImageBasics(_size), Camera<ColorType>(_size), sccb_drv_(std::move(_bus_drv)){;}
 };
 
 };
