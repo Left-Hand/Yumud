b761f3a35da55b118e6c78e52a7374d2ad3a2708
Rstr1aN <745236859@qq.com>
2025-02-14 13:45:56 +0800

    添加了drv8801

---
+++ drivers/Actuator/Bridge/DRV8801/DRV8801.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+
+#include "../CoilDriver.hpp"
+
+namespace ymd::drivers{
+
+class DRV8801:public Coil2DriverIntf{
+protected:
+    PwmChannel & pwm_;
+    GpioConcept & dir_;
+    AnalogInChannel & curr_ch_;
+
+    public:
+    DRV8801(PwmChannel & _pwm, GpioConcept & _dir, AnalogInChannel & _curr_ch):
+        pwm_(_pwm),
+        dir_(_dir),
+        curr_ch_(_curr_ch){;}
+
+
+    // void init();
+    void enable(const bool en){}
+
+
+    __fast_inline DRV8801 & operator = (const real_t duty);
+};
+
+DRV8801 & DRV8801::operator = (const real_t duty){
+    if(duty >= 0){
+        pwm_ = MIN(duty, 1);
+        dir_ = 1;
+    }else{
+        pwm_ = MAX(-duty, -1);
+        dir_ = 0;
+    }
+    return *this;
+}
+
+};
