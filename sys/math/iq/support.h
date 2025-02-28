32ca87e128ce0dc26abec7af299ae3dc5efe1af3
Rstr1aN <745236859@qq.com>
2025-02-28 20:44:32 +0800

    添加了iiq类型

--- sys/math/iq/support.h
+++ sys/math/iq/support.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "rts_support.h"
+#include "_IQN.hpp"
+#include "_IIQN.hpp"
 
 // #define __IQMATH_USE_MY_IMPL
 
@@ -31,44 +33,3 @@
 #define _iq31_pointOne    0x0ccccccd
 #define _iq31_oneTenth    0x0ccccccd
 #define _iq31_one         0x7fffffff
-
-
-template<size_t Q>
-requires (Q < 32)
-struct _iq{
-private:
-    int32_t value_ = 0;
-    __fast_inline constexpr _iq<Q>(const int32_t value):
-        value_(value){
-    }
-public:
-    operator int32_t() = delete;
-
-    static __fast_inline constexpr _iq<Q> from_i32(const int32_t value){
-        return _iq<Q>(value);
-    }
-
-    __fast_inline constexpr int32_t to_i32() const {
-        return value_;
-    }
-
-    __fast_inline constexpr uint32_t to_u32() const {
-        return std::bit_cast<uint32_t>(value_);
-    }
-
-    __fast_inline constexpr bool signbit() const{
-        return value_ & 0x80000000;
-    }
-    __fast_inline constexpr operator bool() const{return value_;}
-
-    template<size_t P>
-    __fast_inline constexpr explicit operator _iq<P>() const{
-        if constexpr (P > Q){
-            return _iq<P>::from_i32(int32_t(value_) << (P - Q));
-        }else if constexpr (P < Q){
-            return _iq<P>::from_i32(int32_t(value_) >> (Q - P));
-        }else{
-            return _iq<P>::from_i32(int32_t(value_));
-        }
-    }
-};
