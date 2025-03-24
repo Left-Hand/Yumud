b88f97ae4c600c2624685f11a48ad2667c3a2c8d
Rstr1aN <745236859@qq.com>
2025-03-24 15:27:52 +0800

    添加了跟踪微分器

--- targets/CH32V20X/src/testbench/basics/ws2812_tb.cpp
+++ targets/CH32V20X/src/testbench/basics/ws2812_tb.cpp
@@ -273,15 +273,16 @@ class SlidingModeController {
 public:
     SlidingModeController(){}
     void update(const q24 targ,const q24 meas) {
-        static constexpr q24 c = 0.06_q24;
-        static constexpr q24 q = 0.0006_q24;
+        static constexpr q24 c = 0.12_q24;
+        static constexpr q24 q = 0.0004_q24;
+        static constexpr q24 e = 0.000027_q24;
 
         const q24 x1 = targ - meas;
         const q24 x2 = x1 - err_prev_.get();
         err_prev_.borrow_mut() = x1;
 
         const q24 s = c * x1 + x2;
-        const q24 delta = c * x2 + q * s; 
+        const q24 delta = c * x2 + q * s + e * sign(s); 
 
         output_.borrow_mut() = CLAMP(output_.get() + delta, out_min_.get(), out_max_.get());
     }
@@ -297,10 +298,308 @@ private:
     immutable_t<q24> err_prev_ = 0;
 };
 
+static constexpr real_t ssqrt(real_t x){
+    return sign(x) * sqrt(ABS(x));
+}
+
+
+
+real_t fal(const real_t e, const real_t alpha, const real_t delta){
+
+    const auto fabsf_e = abs(e);
+    
+    if(delta>=fabsf_e)
+        return e/pow(delta,1-alpha);
+    else
+        return pow(fabsf_e,alpha)*sign(e);
+}
+
+
+template<typename T, size_t N>
+class StateVector{
+public:
+    StateVector(){
+        reset();
+    }
+
+    void reset(){
+        // data_.fill(T(0));
+        for(size_t i=0;i<N;i++) data_[i] = 0;
+    }
+
+    StateVector(const std::initializer_list<T> list){
+        std::copy(list.begin(), list.end(), data_.begin());
+    }
+
+    const T & operator [](const size_t idx){
+        return data_[idx];
+    }
+
+    StateVector & operator += (const StateVector & other){
+        for(size_t i=0;i<N;i++)
+            data_[i] += other.data_[i];
+        return *this;
+    }
+
+    const auto & get() const{return data_;}
+    const T & back() const{return data_.back();}
+private:    
+    std::array<T, N> data_;
+};
+
+class TrackingDifferentiator{
+public:
+    struct Config{
+        q24 h;
+        q24 r;
+        uint fs;
+    };
+
+    TrackingDifferentiator(const Config & cfg){
+        reconf(cfg);
+        reset();
+    }
+
+    void reset(){
+        state_.reset();
+    }
+
+
+    void update(const q24 v){
+        const auto h = h_.get();
+        const auto r = r_.get();
+
+        const auto x1 = state_[0];
+        const auto x2 = state_[1];
+
+        state_ += {
+            h * x2, h * fhan(x1 - v,x2,r, h)
+        };
+    }
+
+    void update2(const q24 v){
+        const auto h = h_.get();
+
+        const auto x1 = state_[0];
+        const auto x2 = state_[1];
+
+        state_ += {
+            h * x2, h * fhan2(x1 - v,x2)
+        };
+    }
+    
+    static constexpr q24 fhan(q24 x1,q24 x2,q24 r,q24 h){
+
+        const auto deltaa = r*h;
+        const auto deltaa0 = deltaa*h;
+        const auto y = x1 + (x2*h);
+        const auto a0 = sqrt(deltaa*deltaa+8*r*abs(y));
+        const auto a = ((abs(y)<=deltaa0) ? (x2+y/h) : (x2+0.5_r*(a0-deltaa)*sign(y)));
+    
+        if(abs(a)<=deltaa)
+            return -r*a/deltaa;
+        else
+            return -r*sign(a);
+    }
+
+    __fast_inline
+    constexpr q24 fhan2(q24 x1,q24 x2) const{
+        const auto deltaa_squ = square(deltaa_.get());
+        const auto r = r_.get();
+        const auto inv_h = inv_h_.get();
+
+        const auto y = x1 + x2 * h_.get();
+        const auto a0 = sqrt(deltaa_squ + 8 * r * abs(y));
+        const auto a = ((abs(y)<=deltaa0_.get()) ? 
+            (x2+y * inv_h) : 
+            (x2+0.5_r*(a0-deltaa_.get())*sign(y)));
+    
+        if(abs(a)<=deltaa_.get())
+            return -a * inv_h;
+        else
+            return -r*sign(a);
+    }
+
+    const auto & get() const {return state_.get();}
+
+
+    void reconf(const Config & cfg){
+        h_.borrow_mut() = cfg.h;
+        r_.borrow_mut() = cfg.r;
+
+        deltaa_.borrow_mut() = cfg.r*cfg.h;
+        deltaa0_.borrow_mut() = cfg.r*cfg.h*cfg.h;
+        inv_h_.borrow_mut() = 1/cfg.h;
+        // d_lmt_.borrow_mut() = - cfg.r 
+    }
+private:
+    immutable_t<q24> h_ = 0;
+    immutable_t<q24> r_ = 0;
+    
+    immutable_t<q24> deltaa_ = 0;
+    immutable_t<q24> deltaa0_ = 0;
+    immutable_t<q24> inv_h_ = 0;
+    // immutable_t<q24> d_lmt_ = 0;
+
+    using State = StateVector<q24, 2>;
+    State state_;
+};
+
+
+template<size_t N>
+class TrackingDifferentiatorByOrders;
+
+template<>
+class TrackingDifferentiatorByOrders<2>{
+public:
+    struct Config{
+        q24 r;
+        uint fs;
+    };
+
+    TrackingDifferentiatorByOrders(const Config & cfg){
+        reconf(cfg);
+        reset();
+    }
+
+    void reset(){
+        state_.reset();
+    }
+
+
+    void update(const q24 u){
+        const auto r = r_.get();
+        const auto r_squ = r * r;
+        const auto inv_fs = inv_fs_.get();
+
+        const auto x1 = state_[0];
+        const auto x2 = state_[1];
+
+        state_ += {
+            x2 * inv_fs, 
+            (- 2 * r * x2 - r_squ * (x1 - u)) * inv_fs
+        };
+    }
+
+
+    void reconf(const Config & cfg){
+        r_.borrow_mut() = cfg.r;
+        inv_fs_.borrow_mut() = 1_q24 / cfg.fs;
+    }
+private:
+    immutable_t<q24> r_ = 0;
+    immutable_t<q24> inv_fs_ = 0;
+
+    using State = StateVector<q24, 2>;
+    State state_;
+};
+
+template<>
+class TrackingDifferentiatorByOrders<3>{
+public:
+    struct Config{
+        q24 r;
+        uint fs;
+    };
+
+    TrackingDifferentiatorByOrders(const Config & cfg){
+        reconf(cfg);
+        reset();
+    }
+
+    void reset(){
+        state_.reset();
+    }
+
+
+    void update(const q24 u){
+        const auto r = r_.get();
+        const auto r_squ = r * r;
+        const auto inv_fs = inv_fs_.get();
+
+        const auto x1 = state_[0];
+        const auto x2 = state_[1];
+        const auto x3 = state_[2];
+
+        state_ += {
+            x2 * inv_fs, 
+            x3 * inv_fs,
+            (- 3 * x3 - 3 * r * x2 - r_squ * (x1 - u)) * r * inv_fs
+        };
+    }
+
+
+    void reconf(const Config & cfg){
+        r_.borrow_mut() = cfg.r;
+        inv_fs_.borrow_mut() = 1_q24 / cfg.fs;
+    }
+private:
+    immutable_t<q24> r_ = 0;
+    immutable_t<q24> inv_fs_ = 0;
+
+    using State = StateVector<q24, 3>;
+    State state_;
+};
+
+
+template<>
+class TrackingDifferentiatorByOrders<4>{
+public:
+    struct Config{
+        q24 r;
+        uint fs;
+    };
+
+    TrackingDifferentiatorByOrders(const Config & cfg){
+        reconf(cfg);
+        reset();
+    }
+
+    void reset(){
+        state_.reset();
+    }
+
+
+    void update(const q24 u){
+        const auto r = r_.get();
+        const auto r_squ = r * r;
+        const auto inv_fs = inv_fs_.get();
+
+        const auto x1 = state_[0];
+        const auto x2 = state_[1];
+        const auto x3 = state_[2];
+        const auto x4 = state_[3];
+
+        state_ += {
+            x2 * inv_fs, 
+            x3 * inv_fs,
+            x3 * inv_fs,
+            (-4 * x4 - 6 * x3 - 4 * r * x2 - r_squ * r * (x1 - u)) * r * inv_fs
+        };
+    }
+
+
+    void reconf(const Config & cfg){
+        r_.borrow_mut() = cfg.r;
+        inv_fs_.borrow_mut() = 1_q24 / cfg.fs;
+    }
+
+    const auto & back() const {return state_.back();}
+private:
+    immutable_t<q24> r_ = 0;
+    immutable_t<q24> inv_fs_ = 0;
+
+    using State = StateVector<q24, 4>;
+    State state_;
+};
+
+
+
 
 [[maybe_unused]] static void at8222_tb(){
     // hal::UartSw uart{portA[5], NullGpio}; uart.init(19200);
     // DEBUGGER.retarget(&uart);
+    DEBUGGER.noBrackets();
 
     // TARG_UART.init(6_MHz);
 
@@ -385,10 +684,15 @@ private:
     // };
 
     SlidingModeController pi_ctrl;
-
+    TrackingDifferentiator td{{
+        .h = 0.002_q24,
+        .r = 5.96_r
+    }};
 
     volatile uint32_t exe_micros = 0;
     real_t spd_targ = 0;
+    real_t pos_targ = 0;
+
     adc1.attach(AdcIT::JEOC, {0,0}, [&](){
         const auto begin_micros = micros();
         const auto volt = adc1.inj(1).get_voltage();
@@ -402,11 +706,21 @@ private:
         curr_mid = lpf_mid.result();
 
         ect.update(bool(bpf.result() > 0));
-        spe.update(ect.count() * 0.01_r);
-        pi_ctrl.update(spd_targ, spe.get());
+
+        const auto pos = ect.count() * 0.01_r;
+        spe.update(pos);
+        const auto spd = spe.get();
+
+        static constexpr auto kp = 87.0_r;
+        static constexpr auto kd = 0.0_r;
+        const auto spd_cmd = kp * ssqrt(pos_targ - pos) + kd * (spd_targ - spd);
+        pi_ctrl.update(spd_cmd, spd);
         pwm_pos = pi_ctrl.get();
         exe_micros = micros() - begin_micros;
 
+        const auto t = time();
+        const auto s = sin(t * 10);
+        td.update(s);
         // uart.tick();
     });
 
@@ -427,12 +741,25 @@ private:
         // pwm_pos = 0.9_r + 0.1_r * sin(5 * time());
         // spd_targ = 7.0_r + 3 * sin(5 * time());
         // spd_targ = 8.0_r + 1.0_r * ((sin(2.0_r * time())) > 0 ? 1 : -1);
-        spd_targ = 8.0_r + 1.0_r * sinpu(2.0_r * time());
-        // spd_targ = 7.0_r + 1.0_r * sign(sin(2.0_r * time()));
+        // spd_targ = 8.0_r + 1.0_r * sinpu(2.0_r * time());
+        const auto t = time();
+
+        #define TEST_MODE 0
+
+        #if TEST_MODE == 0
+        spd_targ = 12;
+        pos_targ = 10.0_r * t + 2*frac(t);
+        #elif TEST_MODE == 1
+        spd_targ = 7.0_r + 1.0_r * sin(6 * t);
+        pos_targ = 7.0_r * t + real_t(-1.0/6) * cos(6 * t);
+        #endif
         // spd_targ = 9.0_r + 1.0_r * ((sin(1.0_r * time())) > 0 ? 1 : ;
         // spd_targ = 9.0_r + 1.0_r * -1;
         // spd_targ = 16.57_r;
-        DEBUG_PRINTLN_IDLE(spd_targ, curr * 10, curr_mid * 10, pi_ctrl.get(), bpf.result() * 10, spe.get(), bool(bpf.result() > 0), exe_micros);
+        // DEBUG_PRINTLN_IDLE(pos_targ, spd_targ, curr * 10,  pi_ctrl.get(), bpf.result(), spe.get(), ect.count() * 0.01_r, exe_micros);
+
+
+        DEBUG_PRINTLN_IDLE(td.get());
         // DEBUG_PRINTLN(duty, bool(pwm_pos.io()), bool(pwm_neg.io()));
         
     }
