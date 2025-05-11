fd5eff88c6f68e09348ed6bbf5148573be6a1359
Rstr1aN <745236859@qq.com>
2025-05-11 18:00:39 +0800

    完成了icm42688的spi读取

--- drivers/IMU/Axis6/ICM42688/icm42688.hpp
+++ drivers/IMU/Axis6/ICM42688/icm42688.hpp
@@ -11,6 +11,8 @@ class ICM42688:
 {
 public:
     ICM42688(hal::I2c & i2c, const hal::I2cSlaveAddr<7> i2c_addr = DEFAULT_I2C_ADDR):phy_(i2c, i2c_addr){;}
+    ICM42688(hal::Spi & spi, const hal::SpiSlaveIndex idx):phy_(spi, idx){;}
+    ICM42688(hal::SpiDrv && spi_drv):phy_(std::move(spi_drv)){;}
 
     IResult<> init();
     
@@ -24,17 +26,76 @@ public:
     Option<Vector3_t<real_t>> get_gyr();
 private:
     InvensenseSensor_Phy phy_;
+    Option<Bank> last_bank_ = None;
 
-    real_t lsb_acc_x64;
-    real_t lsb_gyr_x256;
+    IResult<> switch_bank(const Bank bank){
+        if(last_bank_.is_none() or (last_bank_.unwrap() != bank)){
+            const auto res = phy_.write_reg(0x76, static_cast<uint8_t>(bank));
+            last_bank_ = Some(bank);
+            return res;
+        }
+        return Ok();
+    }
 
-    #pragma pack(push, 1)
-    Vector3_t<int16_t> acc_data_;
-    Vector3_t<int16_t> gyr_data_;
-    #pragma pack(pop)
+    IResult<> write_reg(const auto & reg){
+        if(const auto res = switch_bank(reg.bank);
+            res.is_err()) return res;
+        return phy_.write_reg(reg.address, reg.as_val());
+    }
 
-    static_assert(sizeof(acc_data_) == 6);
-    static_assert(sizeof(gyr_data_) == 6);
+    IResult<> read_reg(auto & reg){
+        if(const auto res = switch_bank(reg.bank);
+            res.is_err()) return res;
+        return phy_.read_reg(reg.address, reg.as_ref());
+    };
+
+    static constexpr q24 calc_gyr_lsb(const GyrFs fs){
+        /*Turn Into Radian*/
+        constexpr  q24 LSB_GYR_2000_R	= q24(0.0010652644);
+        constexpr  q24 LSB_GYR_1000_R	= q24(0.00053263222);
+        constexpr  q24  LSB_GYR_500_R	= q24(0.00026631611);
+        constexpr  q24  LSB_GYR_250_R	= q24(0.00013315805);
+        constexpr  q24 LSB_GYR_125_R  	= q24(0.000066579027);	
+        constexpr  q24 LSB_GYR_62_5_R  	= q24(0.000066579027/2);	
+        constexpr  q24 LSB_GYR_31_25_R  	= q24(0.000066579027/4);	
+        constexpr  q24 LSB_GYR_15_625_R  	= q24(0.000066579027/8);	
+
+        switch(fs){
+            case GyrFs::_2000DPS:   return LSB_GYR_2000_R;
+            case GyrFs::_1000DPS:   return LSB_GYR_1000_R;
+            case GyrFs::_500DPS:    return LSB_GYR_500_R;
+            case GyrFs::_250DPS:    return LSB_GYR_250_R;
+            case GyrFs::_125DPS:    return LSB_GYR_125_R;
+            case GyrFs::_62_5DPS:   return LSB_GYR_62_5_R;
+            case GyrFs::_31_25DPS:  return LSB_GYR_31_25_R;
+            case GyrFs::_15_625DPS: return LSB_GYR_15_625_R;
+            default: __builtin_unreachable();
+            
+        }
+    }
+
+    static constexpr q24 calc_acc_lsb(const AccFs fs){
+
+        constexpr q24 LSB_ACC_16G = q24( 0.0047856934);
+        constexpr q24  LSB_ACC_8G = q24( 0.0023928467);
+        constexpr q24  LSB_ACC_4G = q24( 0.0011964233);
+        constexpr q24  LSB_ACC_2G = q24(0.00059821167);
+        switch(fs){
+            case AccFs::_16G: return LSB_ACC_16G;
+            case AccFs::_8G: return LSB_ACC_8G;
+            case AccFs::_4G: return LSB_ACC_4G;
+            case AccFs::_2G: return LSB_ACC_2G;
+            default: __builtin_unreachable();
+        }
+    }
+
+    IResult<> set_gyr_odr(const GyrOdr odr);
+    IResult<> set_gyr_fs(const GyrFs fs);
+    IResult<> set_acc_odr(const AccOdr odr);
+    IResult<> set_acc_fs(const AccFs fs);
+    
+    q24 lsb_acc_;
+    q24 lsb_gyr_;
 };
 
 }
--- targets/CH32V20X/src/testbench/imu/icm42688_tb.cpp
+++ targets/CH32V20X/src/testbench/imu/icm42688_tb.cpp
@@ -24,8 +24,9 @@ using namespace ymd::drivers;
 static constexpr uint FS = 1000;
 static constexpr auto INV_FS = (1.0_q24 / FS);
 // #define MAG_ACTIVATED
-static 
-void icm42688_tb(hal::I2c & i2c){
+
+[[maybe_unused]]
+static void icm42688_i2c_tb(hal::I2c & i2c){
     ICM42688 imu{i2c};
     imu.init().expect("icm42688 init failed");
 
@@ -41,19 +42,60 @@ void icm42688_tb(hal::I2c & i2c){
         // const auto u0 = micros();
         // imu.update().unwrap();
         // const auto u1 = micros();
-        // DEBUG_PRINTLN(imu.get_acc().unwrap(), imu.get_gyr().unwrap(), u1 - u0);
-        DEBUG_PRINTLN(z);
+        DEBUG_PRINTLN(imu.get_acc().unwrap(), imu.get_gyr().unwrap(), z);
+        // DEBUG_PRINTLN(z);
         
         // DEBUG_PRINTLN(millis());
     }
 }
 
+[[maybe_unused]]
+static void icm42688_spi_tb(hal::SpiDrv && drv){
+    ICM42688 imu{std::move(drv)};
+    // while(true){
+    //     auto test_fn = [&]{
+    //         return drv.write_single<uint8_t>(uint8_t(0), CONT)
+    //         | drv.write_single<uint8_t>(uint8_t(0));
+    //     };
+
+    //     if(const auto res = test_fn();  
+    //         res.is_err()) DEBUG_PRINTLN(res.unwrap_err()); 
+    //     else{
+    //         DEBUG_PRINTLN("ok");
+    //     }
+    //     delay(1);
+    // }
+    DEBUG_PRINTLN("init started");
+
+    imu.init().unwrap();
+
+    q24 z = 0;
+    uint32_t exe = 0;
+    timer1.init(1000);
+    timer1.attach(TimerIT::Update, {0,0},[&]{
+        const auto u0 = micros();
+        imu.update().unwrap();
+        exe = micros() - u0;
+        z = z + INV_FS * imu.get_gyr().unwrap().z;
+    });
+
+    while(true){
+        // const auto u0 = micros();
+        // imu.update().unwrap();
+        // const auto u1 = micros();
+        DEBUG_PRINTLN(imu.get_acc().unwrap(), imu.get_gyr().unwrap(), z, exe);
+        // DEBUG_PRINTLN(z);
+        
+        // DEBUG_PRINTLN(millis());
+    }
+}
 
 void icm42688_main(){
     UART.init(576_KHz);
     DEBUGGER.retarget(&UART);
     DEBUGGER.no_brackets();
     DEBUGGER.force_sync(true);
+
     // I2cSw i2c{portA[12], portA[15]};
     I2cSw i2c{SCL_GPIO, SDA_GPIO};
     // i2c.init(400_KHz);
@@ -62,7 +104,11 @@ void icm42688_main(){
 
     delay(200);
 
-    icm42688_tb(i2c);
+    // icm42688_i2c_tb(i2c);
+
+    auto & spi = spi1;
+    spi.init(18_MHz);
+    icm42688_spi_tb(SpiDrv(spi, spi.attach_next_cs(portA[15]).value()));
     // mpu6500_tb(i2c);
     // ak8963_tb(i2c);
 }
