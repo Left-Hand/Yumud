ced02151a654161d5bce8f834908a624403c2264
Rstr1aN <745236859@qq.com>
2025-07-15 15:00:19 +0800

    使用宏化简了反射声明

--- targets/CH32V20X/src/app/motor/joint_commands.hpp
+++ targets/CH32V20X/src/app/motor/joint_commands.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "core/utils/serde.hpp"
 
 namespace ymd{
@@ -33,79 +35,40 @@ struct SetKpKd{
     bf16 kp;
     bf16 kd;
 };
+
+struct SetKpKiKd{
+    bf16 kp;
+    bf16 ki;
+    bf16 kd;
+};
+
 }
 
 DEF_DERIVE_SERIALIZE_AS_TUPLE(robots::joint_cmds::SetPosition)
-DEF_DERIVE_DEBUG_AS_DISPLAY(robots::joint_cmds::SetPosition)
 DEF_DERIVE_RAW_BYTES_DESERIALIZER(robots::joint_cmds::SetPosition)
-
-template<>
-struct reflecter::MemberPtrReflecter<robots::joint_cmds::SetPosition> {
-    
-    template<size_t N>
-    static constexpr auto member_ptr_v = [] {
-        if constexpr (N == 0) return &robots::joint_cmds::SetPosition::position;
-    }();
-};
+DEF_DERIVE_MEM_REFLECTER_1(robots::joint_cmds::SetPosition, position)
 
 
 DEF_DERIVE_SERIALIZE_AS_TUPLE(robots::joint_cmds::SetSpeed)
-DEF_DERIVE_DEBUG_AS_DISPLAY(robots::joint_cmds::SetSpeed)
 DEF_DERIVE_RAW_BYTES_DESERIALIZER(robots::joint_cmds::SetSpeed)
-
-template<>
-struct reflecter::MemberPtrReflecter<robots::joint_cmds::SetSpeed> {
-    
-    template<size_t N>
-    static constexpr auto member_ptr_v = [] {
-        if constexpr (N == 0) return &robots::joint_cmds::SetSpeed::speed;
-    }();
-};
+DEF_DERIVE_MEM_REFLECTER_1(robots::joint_cmds::SetSpeed, speed)
 
 DEF_DERIVE_SERIALIZE_AS_TUPLE(robots::joint_cmds::SetPositionAndSpeed)
-DEF_DERIVE_DEBUG_AS_DISPLAY(robots::joint_cmds::SetPositionAndSpeed)
 DEF_DERIVE_RAW_BYTES_DESERIALIZER(robots::joint_cmds::SetPositionAndSpeed)
-
-template<>
-struct reflecter::MemberPtrReflecter<robots::joint_cmds::SetPositionAndSpeed> {
-    
-    template<size_t N>
-    static constexpr auto member_ptr_v = [] {
-        if constexpr (N == 0) return &robots::joint_cmds::SetPositionAndSpeed::position;
-        else if constexpr (N == 1) return &robots::joint_cmds::SetPositionAndSpeed::speed;
-    }();
-};
-
+DEF_DERIVE_MEM_REFLECTER_2(robots::joint_cmds::SetPositionAndSpeed, position, speed)
 
 
 DEF_DERIVE_SERIALIZE_AS_TUPLE(robots::joint_cmds::SetTrapzoid)
-DEF_DERIVE_DEBUG_AS_DISPLAY(robots::joint_cmds::SetTrapzoid)
 DEF_DERIVE_RAW_BYTES_DESERIALIZER(robots::joint_cmds::SetTrapzoid)
-
-template<>
-struct reflecter::MemberPtrReflecter<robots::joint_cmds::SetTrapzoid> {
-    
-    template<size_t N>
-    static constexpr auto member_ptr_v = [] {
-        if constexpr (N == 0) return &robots::joint_cmds::SetTrapzoid::position;
-        else if constexpr (N == 1) return &robots::joint_cmds::SetTrapzoid::speed;
-        else if constexpr (N == 2) return &robots::joint_cmds::SetTrapzoid::acceleration;
-        else if constexpr (N == 3) return &robots::joint_cmds::SetTrapzoid::deceleration;
-    }();
-};
+DEF_DERIVE_MEM_REFLECTER_4(robots::joint_cmds::SetTrapzoid, position, speed, acceleration, deceleration)
 
 DEF_DERIVE_SERIALIZE_AS_TUPLE(robots::joint_cmds::SetKpKd)
-DEF_DERIVE_DEBUG_AS_DISPLAY(robots::joint_cmds::SetKpKd)
+DEF_DERIVE_RAW_BYTES_DESERIALIZER(robots::joint_cmds::SetKpKd)
+DEF_DERIVE_MEM_REFLECTER_2(robots::joint_cmds::SetKpKd, kp, kd)
 
-template<>
-struct reflecter::MemberPtrReflecter<robots::joint_cmds::SetKpKd> {
-    
-    template<size_t N>
-    static constexpr auto member_ptr_v = [] {
-        if constexpr (N == 0) return &robots::joint_cmds::SetKpKd::kp;
-        else if constexpr (N == 1) return &robots::joint_cmds::SetKpKd::kd;
-    }();
-};
+DEF_DERIVE_SERIALIZE_AS_TUPLE(robots::joint_cmds::SetKpKiKd)
+DEF_DERIVE_RAW_BYTES_DESERIALIZER(robots::joint_cmds::SetKpKiKd)
+DEF_DERIVE_MEM_REFLECTER_3(robots::joint_cmds::SetKpKiKd, kp, ki, kd)
 
 
 }
--- targets/CH32V20X/src/app/motor/machine_commands.hpp
+++ targets/CH32V20X/src/app/motor/machine_commands.hpp
@@ -43,77 +43,30 @@ struct Abort{
 DEF_DERIVE_SERIALIZE_AS_TUPLE(robots::machine_cmds::SetPositionXy)
 DEF_DERIVE_DEBUG_AS_DISPLAY(robots::machine_cmds::SetPositionXy)
 DEF_DERIVE_RAW_BYTES_DESERIALIZER(robots::machine_cmds::SetPositionXy)
+DEF_DERIVE_MEM_REFLECTER_2(robots::machine_cmds::SetPositionXy, x, y)
 
-template<>
-struct reflecter::MemberPtrReflecter<robots::machine_cmds::SetPositionXy> {
-    
-    template<size_t N>
-    static constexpr auto member_ptr_v = [] {
-        if constexpr (N == 0) return &robots::machine_cmds::SetPositionXy::x;
-        else if constexpr (N == 1) return &robots::machine_cmds::SetPositionXy::y;
-    }();
-};
 
 DEF_DERIVE_SERIALIZE_AS_TUPLE(robots::machine_cmds::SetPositionXyz)
 DEF_DERIVE_DEBUG_AS_DISPLAY(robots::machine_cmds::SetPositionXyz)
 DEF_DERIVE_RAW_BYTES_DESERIALIZER(robots::machine_cmds::SetPositionXyz)
+DEF_DERIVE_MEM_REFLECTER_3(robots::machine_cmds::SetPositionXyz, x, y, z)
 
-template<>
-struct reflecter::MemberPtrReflecter<robots::machine_cmds::SetPositionXyz> {
-    
-    template<size_t N>
-    static constexpr auto member_ptr_v = [] {
-        if constexpr (N == 0) return &robots::machine_cmds::SetPositionXyz::x;
-        else if constexpr (N == 1) return &robots::machine_cmds::SetPositionXyz::y;
-        else if constexpr (N == 2) return &robots::machine_cmds::SetPositionXyz::z;
-    }();
-};
 
 DEF_DERIVE_SERIALIZE_AS_TUPLE(robots::machine_cmds::Replace)
 DEF_DERIVE_DEBUG_AS_DISPLAY(robots::machine_cmds::Replace)
 DEF_DERIVE_RAW_BYTES_DESERIALIZER(robots::machine_cmds::Replace)
-
-template<>
-struct reflecter::MemberPtrReflecter<robots::machine_cmds::Replace> {
-    
-    template<size_t N>
-    static constexpr auto member_ptr_v = [] {
-        if constexpr (N == 0) return &robots::machine_cmds::Replace::x1;
-        else if constexpr (N == 1) return &robots::machine_cmds::Replace::y1;
-        else if constexpr (N == 2) return &robots::machine_cmds::Replace::x2;
-        else if constexpr (N == 3) return &robots::machine_cmds::Replace::y2;
-    }();
-};
+DEF_DERIVE_MEM_REFLECTER_4(robots::machine_cmds::Replace, x1, y1, x2, y2)
 
 
 DEF_DERIVE_SERIALIZE_AS_TUPLE(robots::machine_cmds::Spin)
 DEF_DERIVE_DEBUG_AS_DISPLAY(robots::machine_cmds::Spin)
 DEF_DERIVE_RAW_BYTES_DESERIALIZER(robots::machine_cmds::Spin)
-
-template<>
-struct reflecter::MemberPtrReflecter<robots::machine_cmds::Spin> {
-    
-    template<size_t N>
-    static constexpr auto member_ptr_v = [] {
-        if constexpr (N == 0) return &robots::machine_cmds::Spin::delta_rotation;
-    }();
-};
-
-
+DEF_DERIVE_MEM_REFLECTER_1(robots::machine_cmds::Spin, delta_rotation)
 
 
 DEF_DERIVE_SERIALIZE_AS_TUPLE(robots::machine_cmds::Rotate)
 DEF_DERIVE_DEBUG_AS_DISPLAY(robots::machine_cmds::Rotate)
 DEF_DERIVE_RAW_BYTES_DESERIALIZER(robots::machine_cmds::Rotate)
-
-template<>
-struct reflecter::MemberPtrReflecter<robots::machine_cmds::Rotate> {
-    
-    template<size_t N>
-    static constexpr auto member_ptr_v = [] {
-        if constexpr (N == 0) return &robots::machine_cmds::Rotate::rotation;
-    }();
-};
-
+DEF_DERIVE_MEM_REFLECTER_1(robots::machine_cmds::Rotate, rotation)
 
 }
