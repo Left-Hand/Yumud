1190b06773050781545613c1053e38662cb23602
Rstr1aN <745236859@qq.com>
2025-08-20 17:57:12 +0800

    添加了hc32的寄存器描述

---
+++ ral/hc32/hc32f460_timer_a_def.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cstdint>
+#include <bit>
+
+namespace ymd::ral::hc32f460{
+
+}
---
+++ ral/hc32/hc32f4a0_hrtimer_def.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstdint>
+
+namespace ymd::ral::ch32f4a0{
+
+struct HRPWM_CRn{
+    uint32_t NSEL:8;
+    uint32_t PSEL:8;
+    uint32_t :13;
+    uint32_t NE:1;
+    uint32_t PE:1;
+    uint32_t EN:1;
+};
+
+struct HRPWM_CALCRn{
+    uint32_t CALCODE:8;
+    uint32_t :4;
+    uint32_t ENDF:1;
+    uint32_t :2;
+    uint32_t CALEN:1;
+    uint32_t :16;
+};
+
+}
