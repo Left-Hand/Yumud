59e3ba29f38f66cb8c39ace62bc1fdf50e9696ad
Rstr1aN <745236859@qq.com>
2025-08-20 19:25:12 +0800

    整理文件

---
+++ core/math/iq/iiq_t.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "iq_t.hpp"
+
+
+namespace ymd{
+
+template<size_t Q>
+struct iiq_t{
+    int64_t data;
+};
+}
---
+++ core/math/iq/uiq_t.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "iq_t.hpp"
+
+
+namespace ymd{
+
+template<size_t Q>
+struct uiq_t{
+    int64_t data;
+};
+}
