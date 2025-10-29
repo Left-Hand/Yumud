ff0d8713cbb6801a80c2a61653d4a87a75a49596
Rstr1aN <745236859@qq.com>
2025-10-29 14:28:47 +0800

    完成了ld19的实现

--- drivers/Proximeter/LD19/LD19.hpp
+++ drivers/Proximeter/LD19/LD19.hpp
@@ -9,81 +9,92 @@
 
 #include "hal/bus/uart/uart.hpp"
 #include "core/math/real.hpp"
+#include "core/utils/sumtype.hpp"
 
-namespace ymd::hal{
-class UartDrv{
-public:
-    UartDrv(hal::Uart & uart):
-        uart_(uart){;}
-
-    size_t available(){
-        return uart_.available();
-    }
-    uint16_t read(){
-        char data;
-        uart_.read1(data);
-        return uint32_t(data);
-    }
-private:
-    hal::Uart & uart_;
-};
-}
 
 namespace ymd::drivers{
 
 
 
-class LD19_Phy final{
-public:
-    LD19_Phy(hal::Uart & uart):
-        uart_drv_(uart){;}
-private:
-    hal::UartDrv uart_drv_;
-};
 
 
-class LD19 final{
-public:
-    static constexpr uint8_t HEADER_TOKEN = 0x54;
-    static constexpr uint16_t VERLEN_TOKEN = 0x2C;
-    static constexpr size_t POINTS_PER_FRAME = 12;
+struct LD19_Prelude{
+
     static constexpr size_t DEFAULT_UART_BAUD = 230400;
 
-    struct LidarPoint{
+    static constexpr uint8_t HEADER_TOKEN = 0x54;
+    static constexpr uint8_t VERLEN_TOKEN = 0x2C;
+    static constexpr size_t POINTS_PER_FRAME = 12;
+
+    struct [[nodiscard]] LidarPoint{
         uint16_t distance_mm;
         uint8_t intensity;
 
-        real_t distance() const {
+        [[nodiscard]] constexpr q16 distance() const {
             return distance_mm * 0.001_r;
         }
     };
 
-    class LidarSpinSpeed{
+    struct LidarSpinSpeed{
     public:
         LidarSpinSpeed(uint16_t data):data_(data){;}
 
-        real_t to_radian() const{
-            static constexpr real_t LSB_TO_RADIAN = real_t(TAU / 360);
-            return LSB_TO_RADIAN * data_;
+        [[nodiscard]] constexpr q16 to_turns_per_secs() const{
+            constexpr q16 RATIO = q16(1.0 / 360);
+            return RATIO * data_;
         }
     private:
         uint16_t data_;
     };
 
-    class LidarAngle{
+    struct LidarAngle{
     public:
         LidarAngle(uint16_t data):data_(data){;}
 
-        real_t to_radian() const{
-            static constexpr auto LSB_TO_RADIAN = q24(TAU / 360 * 0.01);
-            return LSB_TO_RADIAN * data_;
+        [[nodiscard]] constexpr q16 to_turns() const{
+            constexpr auto RATIO = q24(1 / 360 * 0.01);
+            return RATIO * data_;
         }
 
     private:
         uint16_t data_;
     };
 
+
+    static constexpr std::array<uint8_t, 256> CRC8_TABLE= {
+        0x00,0x4d,0x9a,0xd7,0x79,0x34,0xe3,
+        0xae,0xf2,0xbf,0x68,0x25,0x8b,0xc6,0x11,0x5c,0xa9,0xe4,0x33,
+        0x7e,0xd0,0x9d,0x4a,0x07,0x5b,0x16,0xc1,0x8c,0x22,0x6f,0xb8,
+        0xf5,0x1f,0x52,0x85,0xc8,0x66,0x2b,0xfc,0xb1,0xed,0xa0,0x77,
+        0x3a,0x94,0xd9,0x0e,0x43,0xb6,0xfb,0x2c,0x61,0xcf,0x82,0x55,
+        0x18,0x44,0x09,0xde,0x93,0x3d,0x70,0xa7,0xea,0x3e,0x73,0xa4,
+        0xe9,0x47,0x0a,0xdd,0x90,0xcc,0x81,0x56,0x1b,0xb5,0xf8,0x2f,
+        0x62,0x97,0xda,0x0d,0x40,0xee,0xa3,0x74,0x39,0x65,0x28,0xff,
+        0xb2,0x1c,0x51,0x86,0xcb,0x21,0x6c,0xbb,0xf6,0x58,0x15,0xc2,
+        0x8f,0xd3,0x9e,0x49,0x04,0xaa,0xe7,0x30,0x7d,0x88,0xc5,0x12,
+        0x5f,0xf1,0xbc,0x6b,0x26,0x7a,0x37,0xe0,0xad,0x03,0x4e,0x99,
+        0xd4,0x7c,0x31,0xe6,0xab,0x05,0x48,0x9f,0xd2,0x8e,0xc3,0x14,
+        0x59,0xf7,0xba,0x6d,0x20,0xd5,0x98,0x4f,0x02,0xac,0xe1,0x36,
+        0x7b,0x27,0x6a,0xbd,0xf0,0x5e,0x13,0xc4,0x89,0x63,0x2e,0xf9,
+        0xb4,0x1a,0x57,0x80,0xcd,0x91,0xdc,0x0b,0x46,0xe8,0xa5,0x72,
+        0x3f,0xca,0x87,0x50,0x1d,0xb3,0xfe,0x29,0x64,0x38,0x75,0xa2,
+        0xef,0x41,0x0c,0xdb,0x96,0x42,0x0f,0xd8,0x95,0x3b,0x76,0xa1,
+        0xec,0xb0,0xfd,0x2a,0x67,0xc9,0x84,0x53,0x1e,0xeb,0xa6,0x71,
+        0x3c,0x92,0xdf,0x08,0x45,0x19,0x54,0x83,0xce,0x60,0x2d,0xfa,
+        0xb7,0x5d,0x10,0xc7,0x8a,0x24,0x69,0xbe,0xf3,0xaf,0xe2,0x35,
+        0x78,0xd6,0x9b,0x4c,0x01,0xf4,0xb9,0x6e,0x23,0x8d,0xc0,0x17,
+        0x5a,0x06,0x4b,0x9c,0xd1,0x7f,0x32,0xe5,0xa8
+    };
+    static constexpr uint8_t calc_crc8(const std::span<const uint8_t> pbuf){
+        uint8_t crc = 0;
+        for(size_t i = 0; i < pbuf.size(); i++){
+            crc = CRC8_TABLE[(crc ^ pbuf[i])&0xff];
+        }
+        return crc;
+    }
+
     struct LidarFrame final{
+        using Self = LidarFrame;
 
         #pragma pack(push, 1)
         uint8_t header;
@@ -95,10 +106,64 @@ public:
         uint16_t timestamp;
         uint8_t crc8;
         #pragma pack(pop)
-    public:
-        uint8_t calc_crc() const;
+
+        [[nodiscard]] constexpr uint8_t calc_crc() const {
+            constexpr size_t PAYLOAD_LEN = sizeof(Self) - sizeof(crc8);
+            return calc_crc8(std::span<const uint8_t>(&(this->header), PAYLOAD_LEN));
+        }
     };
-private:
-    LD19_Phy phy_;
+
+    static constexpr size_t FRAME_SIZE = sizeof(LidarFrame);
+
+    struct Events{
+        struct FrameReady{
+            const LidarFrame & frame;
+        };
+        struct InvalidCrc{
+            uint8_t expected;
+            uint8_t actual;
+        };
+    };
+
+    struct Event:public Sumtype<Events::FrameReady, Events::InvalidCrc> {
+
+    };
+
+    using Callback = std::function<void(Event)>;
 };
+
+
+class LD19_StreamDecoder final:public LD19_Prelude{
+    explicit LD19_StreamDecoder(Callback callback):
+        callback_(callback)
+    {
+        reset();
+    }
+
+    void push_byte(const uint8_t byte);
+
+    void push_bytes(const std::span<const uint8_t> bytes);
+
+    void flush();
+
+    void reset();
+private:
+
+    Callback callback_;
+    union{
+        LidarFrame frame_;
+        std::array<uint8_t, sizeof(LidarFrame)> bytes_;
+    };
+    size_t bytes_count_ = 0;
+
+    enum class State:uint8_t{
+        WaitingHeader,
+        WaitingVerlen,
+        Remaining
+    };
+
+    State state_ = State::WaitingHeader;
+
+};
+
 }
