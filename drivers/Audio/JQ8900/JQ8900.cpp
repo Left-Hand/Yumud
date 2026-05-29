70e990af016759cd7d73e95080b07b93e26c11ac
Rstr1aN <745236859@qq.com>
2026-05-29 13:49:54 +0800

    添加了新文件

--- drivers/Audio/JQ8900/JQ8900.cpp
+++ drivers/Audio/JQ8900/JQ8900.cpp
@@ -4,7 +4,7 @@
 
 using namespace ymd::drivers;
 
-void JQ8900_Transport::write_byte(const uint8_t data){
+void JQ8900_Transport::blocking_write_byte(const uint8_t data){
     pin_.set_high();
     clock::delay(1000us);
     pin_.set_low();
@@ -27,16 +27,16 @@ void JQ8900_Transport::write_byte(const uint8_t data){
 
 void JQ8900::send_num(const int value){
     if(value > 10){
-        transport_.write_byte(value / 10);
-        transport_.write_byte(value % 10);
+        transport_.blocking_write_byte(value / 10);
+        transport_.blocking_write_byte(value % 10);
     }else{
-        transport_.write_byte(value);
+        transport_.blocking_write_byte(value);
     }
 }
 
 void JQ8900::send_line(const uint8_t value, const Command cmd){
-    transport_.write_byte(static_cast<uint8_t>(Command::Clear));
+    transport_.blocking_write_byte(static_cast<uint8_t>(Command::Clear));
     send_num(value);
-    transport_.write_byte(static_cast<uint8_t>(cmd));
+    transport_.blocking_write_byte(static_cast<uint8_t>(cmd));
 }
 
---
+++ drivers/Audio/yx5300/yx5300.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace ymd::drivers{
+
+struct YX5300{
+
+};
+
+}
