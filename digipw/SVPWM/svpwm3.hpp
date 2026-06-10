b202b6a4aeeee3cedd0aaf675903f547e560442d
Rstr1aN <745236859@qq.com>
2026-06-10 16:34:46 +0800

    更新了电机控制器代码

--- digipw/SVPWM/svpwm3.hpp
+++ digipw/SVPWM/svpwm3.hpp
@@ -6,6 +6,15 @@
 namespace ymd::digipw{
 
 
+enum class [[nodiscard]] SvmSector:uint8_t{
+    _1 = 0b010,
+    _2 = 0b000,
+    _3 = 0b100,
+    _4 = 0b101,
+    _5 = 0b111,
+    _6 = 0b011
+};
+
 struct alignas(16) [[nodiscard]] SvmIntermediate final{
 
     using Self = SvmIntermediate;
@@ -16,24 +25,14 @@ struct alignas(16) [[nodiscard]] SvmIntermediate final{
     iq16 alpha_dutycycle;
     iq16 beta_dutycycle_by_sqrt3;
 
-
-    enum class [[nodiscard]] Sector:uint32_t{
-        _1 = 0b010,
-        _2 = 0b000,
-        _3 = 0b100,
-        _4 = 0b101,
-        _5 = 0b111,
-        _6 = 0b011
-    };
-
-    Sector sector;
+    SvmSector sector;
 
     __attribute__((optimize("Ofast"), always_inline, hot, flatten))
     static constexpr Self from(const AlphaBetaCoord<iq16> alphabeta_dutycycle){
         const auto [alpha_dutycycle, beta_dutycycle] = alphabeta_dutycycle;
         const auto beta_dutycycle_by_sqrt3 = beta_dutycycle * ONE_BY_SQRT3;
     
-        const auto sector = Sector{static_cast<uint32_t>(
+        const auto sector = SvmSector{static_cast<uint8_t>(
             (  static_cast<uint32_t>(std::signbit(beta_dutycycle_by_sqrt3 + alpha_dutycycle)) << 2)
             | (static_cast<uint32_t>(std::signbit(beta_dutycycle_by_sqrt3 - alpha_dutycycle)) << 1)
             | (static_cast<uint32_t>(std::signbit(beta_dutycycle_by_sqrt3)))
@@ -47,54 +46,53 @@ struct alignas(16) [[nodiscard]] SvmIntermediate final{
     }
 
     __attribute__((optimize("Ofast"), always_inline, hot, flatten))
-    constexpr UvwCoord<iq16> to_uvw_dutycycle() const noexcept {
+    constexpr UvwCoord<uq16> to_uvw_dutycycle() const noexcept {
         switch(sector){
-            case Sector::_1:
-            case Sector::_4:
+            case SvmSector::_1:
+            case SvmSector::_4:
             {
 
                 const iq16 a = (alpha_dutycycle - beta_dutycycle_by_sqrt3) >> 1;
                 const iq16 b = beta_dutycycle_by_sqrt3;
 
-                const iq16 u = (HALF_ONE + a + b);
-                const iq16 v = (HALF_ONE - a + b);
-                const iq16 w = (HALF_ONE - a - b);
+                const uq16 u = uq16(HALF_ONE + a + b);
+                const uq16 v = uq16(HALF_ONE - a + b);
+                const uq16 w = uq16(HALF_ONE - a - b);
 
                 return {u, v, w};
             }
 
-            case Sector::_2:
-            case Sector::_5:
+            case SvmSector::_2:
+            case SvmSector::_5:
             {
-                const iq16 u = HALF_ONE + alpha_dutycycle;
-                const iq16 v = HALF_ONE + beta_dutycycle_by_sqrt3;
-                const iq16 w = HALF_ONE - beta_dutycycle_by_sqrt3;
+                const uq16 u = uq16(HALF_ONE + alpha_dutycycle);
+                const uq16 v = uq16(HALF_ONE + beta_dutycycle_by_sqrt3);
+                const uq16 w = uq16(HALF_ONE - beta_dutycycle_by_sqrt3);
 
                 return {u, v, w};
             }
 
-            case Sector::_3:
-            case Sector::_6:
+            case SvmSector::_3:
+            case SvmSector::_6:
             {
                 const iq16 a = beta_dutycycle_by_sqrt3;
                 const iq16 b = (- alpha_dutycycle - beta_dutycycle_by_sqrt3) >> 1;
 
-                const iq16 u = (HALF_ONE - a - b);
-                const iq16 v = (HALF_ONE + a + b);
-                const iq16 w = (HALF_ONE - a + b);
+                const uq16 u = uq16(HALF_ONE - a - b);
+                const uq16 v = uq16(HALF_ONE + a + b);
+                const uq16 w = uq16(HALF_ONE - a + b);
 
                 return {u, v, w};
             }
 
-            default:
-                __builtin_unreachable();
         }
+        __builtin_unreachable();
     }
 };
 
 
 // 输入的幅值的临界模长不能超过sqrt(3)/2 此时恰好能使其中一路到达最大内切圆
-static constexpr UvwCoord<iq16> SVM(
+static constexpr UvwCoord<uq16> SVM(
     const AlphaBetaCoord<iq16> alphabeta_dutycycle
 ){
     return SvmIntermediate::from(alphabeta_dutycycle).to_uvw_dutycycle();
